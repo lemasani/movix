30ea5a8184ddb2ed096ab1e21702922fe198a1a3
lemasani <brianlemasan@gmail.com>
2024-10-22 11:45:13 +0300

    Refactor home page to improve trending movies and series section

--- app/page.tsx
+++ app/page.tsx
@@ -13,34 +13,37 @@ export default function Home() {
         <section className="hero h-[500px]">
           <HeroCarousel />
         </section>
-        <section className='container mx-auto mt-10 trending-movies-shows'>
-          <h1 className='text-2xl font-bold'>Trending</h1>
-          <div className="toggle-option flex gap-2 mt-4">
-            <button
-              className={`px-4 py-2 rounded ${view === 'movies' ? 'bg-blue-500 text-white' : 'bg-gray-200'}`}
-              onClick={() => setView('movies')}
-            >
-              Movies
-            </button>
-            <button
-              className={`px-4 py-2 rounded ${view === 'series' ? 'bg-blue-500 text-white' : 'bg-gray-200'}`}
-              onClick={() => setView('series')}
-            >
-              Series
-            </button>
-          </div>
-          <div className="trending-content mt-10">
-            {view === 'movies' ?(
-              <>
-                <h2 className='text-xl italic font-light'>Trending Movies</h2>
-                <MovieCard />
-              </>
-            ): (
-              <>
-                <h2 className='text-xl italic font-light'>Trending Series</h2>
-                <SeriesCard />
-              </>
-            )}
+        <section className='container mt-1 trending-movies-shows'>
+          <div className="container mx-auto  px-5">
+            <h1 className='text-2xl font-bold'>Trending</h1>
+            <div className="toggle-option flex gap-2 mt-4">
+              <button
+                className={`px-4 py-2 rounded ${view === 'movies' ? 'bg-blue-500 text-white' : 'bg-gray-200'}`}
+                onClick={() => setView('movies')}
+              >
+                Movies
+              </button>
+              <button
+                className={`px-4 py-2 rounded ${view === 'series' ? 'bg-blue-500 text-white' : 'bg-gray-200'}`}
+                onClick={() => setView('series')}
+              >
+                Series
+              </button>
+            </div>
+            <div className="trending-content mt-10">
+              {view === 'movies' ?(
+                <>
+                  <h2 className='text-xl italic font-light'>Trending Movies</h2>
+                  <MovieCard />
+                </>
+              ): (
+                <>
+                  <h2 className='text-xl italic font-light'>Trending Series</h2>
+                  <SeriesCard />
+                </>
+              )}
+            </div>
+
           </div>
         </section>
       </main>
